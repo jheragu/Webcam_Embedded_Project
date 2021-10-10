4bfcf5c043d633aaea672fe37d9a331b9b029d08
jheragu <jesusmaled@gmail.com>
2021-10-10 18:25:37 +0000

    first commit

---
+++ curl.sh
@@ -0,0 +1,2 @@
+curl -X PUT -T ./frames/frame_383.jpg -H "x-ms-date: $(date -u)" -H "x-ms-blob-type: BlockBlob" -H "Content-Length: 0" https://videoframesstorage.blob.core.windows.net/frames?sp=racwdl&st=2021-10-10T13:47:31Z&se=2021-12-31T22:47:31Z&spr=https&sv=2020-08-04&sr=c&sig=aEqtBNzI4D8w4BpSCEiJtzyolY7DZil5HTeWIQrk%2FJw%3D
+
---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+#include <curl/curl.h>
+
+using namespace cv;
+using namespace std;
+
+int main(int, char**)
+{
+    Mat frame;
+    //--- INITIALIZE VIDEOCAPTURE
+    VideoCapture cap;
+    // open the default camera using default API
+    // cap.open(0);
+    // OR advance usage: select any API backend
+    int deviceID = 0;             // 0 = open default camera
+    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+    
+    // open selected camera using selected API
+    cap.open(deviceID, apiID);
+
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 220); // valueX = your wanted width
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 110); // valueY = your wanted heigth
+    
+    // check if we succeeded
+    if (!cap.isOpened()) {
+        cerr << "ERROR! Unable to open camera\n";
+        return -1;
+    }
+
+    //timer
+    time_t timer;
+    time_t timer_1;
+    
+    //--- GRAB AND WRITE LOOP
+    cout << "Start saving frames" << endl;
+    
+    //Time now
+    time(&timer);
+    cout << timer << "\n";  
+
+    for (;;)
+    {   
+        auto i = 0; //Avoid several frames being stored --> loop is executed faster than 1s
+        while(((time(&timer_1) - timer) % 5) == 0){
+            
+            cout << timer_1 << "\n"; 
+            // wait for a new frame from camera and store it into 'frame'
+            cap.read(frame);
+            // check if we succeeded
+            if (frame.empty()) {
+                cerr << "ERROR! blank frame grabbed\n";
+                break;
+            }
+            // show live and wait for a key with timeout long enough to show images
+            imshow("Live Camera Feed", frame);
+
+        
+            //Pretty Time Print
+            cout << std::asctime(std::localtime(&timer));
+
+            if(i ==0){
+                long guid = rand() % 1000;
+                imwrite("./frames/frame_" + to_string(guid) + ".jpg", frame);
+
+                
+            }
+            i++;
+        }
+    }
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
+}
